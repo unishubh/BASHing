3b22c7b39503a7039039572d926de38301625a9b
Ojaswa Sharma <ojaswa1942@gmail.com>
2019-02-26 21:59:05 +0530

    Added options using getopts

--- displayoff.sh
+++ displayoff.sh
@@ -1,2 +1,29 @@
+
 #!/bin/bash
-sleep 1; xset dpms force off
+while getopts ":l-:" opt; do
+  case $opt in
+    l)
+  		sleep 1;
+  		xdg-screensaver lock
+  		exit 1
+      ;;
+  	-)
+		case "${OPTARG}" in
+            lock)
+				sleep 1;
+		  		xdg-screensaver lock
+		  		exit 1
+            ;;
+            *)
+				echo "Invalid option: --$OPTARG" >&2
+				exit 1
+			;;
+		esac;;
+    \?)
+		echo "Invalid option: -$OPTARG" >&2
+		exit 1
+      ;;
+  esac
+done
+sleep 1; 
+xset dpms force off
